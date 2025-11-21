8362759a2bcc675c49d268557e1bcafa6f063876
magui <mpsimoes@ua.pt>
2025-11-21 21:15:42 +0000

    fixed the shared_mem.c (syntax errors) and put something on the index.html cause why not

--- src/shared_mem.c
+++ src/shared_mem.c
@@ -12,17 +12,22 @@
 shared_data_t* create_shared_memory() {
     int shm_fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
     if (shm_fd == -1) return NULL;
+    
     if (ftruncate(shm_fd, sizeof(shared_data_t)) == -1) {
-    close(shm_fd);
-    return NULL;
-}
+        close(shm_fd);
+        return NULL;
+    }
 
-shared_data_t* data = mmap(NULL, sizeof(shared_data_t),PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
-close(shm_fd);
-if (data == MAP_FAILED) return NULL;
+    shared_data_t* data = mmap(NULL, sizeof(shared_data_t),PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    
+    close(shm_fd);
+
+    if (data == MAP_FAILED) return NULL;
+
     memset(data, 0, sizeof(shared_data_t));
     return data;
 }
+
 
 void destroy_shared_memory(shared_data_t* data) {
     munmap(data, sizeof(shared_data_t));
